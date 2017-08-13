04238f7e8eeb4643bc4e3476836ca9f001a2f679
PengfeiGuo <626138959@qq.com>
2017-08-13 11:21:13 +0800

    完成首页

--- src/component/index/index.vue
+++ src/component/index/index.vue
@@ -1,12 +1,18 @@
 <template>
-  <section>
-      <p>这是app的首页</p>
-  </section>
+  <article class="index">
+      <v-swipe></v-swipe>
+      <v-nav></v-nav>
+  </article>
 </template>
 
 <script>
+import Cswipe from './swipe.vue';
+import Cnav from './nav.vue';
 export default {
-  
+  components: {
+    'v-swipe': Cswipe,
+    'v-nav': Cnav
+  }
 }
 </script>
 
---
+++ src/component/index/nav.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="nav">
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+        <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-extra mui-icon-extra-new"></span>
+                <div class="mui-media-body">新闻咨询</div></a></li>
+        <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-image"></span>
+                <div class="mui-media-body">图片分享</div></a></li>
+        <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-extra mui-icon-extra-gift"></span>
+                <div class="mui-media-body">商品购买</div></a></li>
+        <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-search"></span>
+                <div class="mui-media-body">搜一搜</div></a></li>
+        <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-phone"></span>
+                <div class="mui-media-body">联系我们</div></a></li>
+        <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-gear"></span>
+                <div class="mui-media-body">设置</div></a></li>
+        <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-info"></span>
+                <div class="mui-media-body">关于我们</div></a></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
