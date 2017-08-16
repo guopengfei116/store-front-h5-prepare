76c560af862ebb4d423b533fd91d2d2defdc8f67
PengfeiGuo <626138959@qq.com>
2017-08-16 10:26:48 +0800

    完成商品列表页

--- src/component/goods/list.vue
+++ src/component/goods/list.vue
@@ -1,63 +1,76 @@
 <template>
   <article class="goods-list">
+    <v-title :title="title"></v-title>
     <ul class="mui-table-view mui-grid-view">
       <!-- 商品详情 -->
-      <li class="mui-table-view-cell mui-media mui-col-xs-6">
+      <li class="mui-table-view-cell mui-media mui-col-xs-6" v-for="item in list" :key="item.id">
       	<router-link to="/">
 	        <div class="mui-card">
 	          <!-- 商品图片 -->
 	          <div class="mui-card-header">
-	          	<img src="https://img14.360buyimg.com/n1/s450x450_jfs/t6859/270/720868896/241631/c96fb342/59795016N17ef8bc4.jpg" alt="" />
+	          	<img :src="item.img_url"/>
 	          </div>
 	          <!-- 商品描述 -->
 	          <div class="mui-card-footer ">
-	            <p class="mui-ellipsis-2">大范甘迪水电费斯蒂芬森分适当放松的的否是方式方式的否是放是</p>
+	            <p class="mui-ellipsis-2">{{ item.title }}</p>
 	          </div>
 	          <!-- 商品价格 -->
 	          <div class="mui-card-content">
 	            <p class="price">
-	            	<span>￥233</span>
-	            	<s>￥2342</s>
+	            	<span>￥{{ item.sell_price }}</span>
+	            	<s>￥{{ item.market_price }}</s>
 	            </p>
 	            <p class="tip">
 	            	<span>热卖中</span>
-	            	<span>剩余5654件</span>
+	            	<span>剩余{{ item.stock_quantity }}件</span>
 	            </p>
 	          </div>
 	        </div>
         </router-link>
       </li>
-      <!-- 商品详情 -->
-      <li class="mui-table-view-cell mui-media mui-col-xs-6">
-        <div class="mui-card">
-          <!-- 商品图片 -->
-          <div class="mui-card-header">
-          	<img src="https://img14.360buyimg.com/n1/s450x450_jfs/t6859/270/720868896/241631/c96fb342/59795016N17ef8bc4.jpg" alt="" />
-          </div>
-          <!-- 商品描述 -->
-          <div class="mui-card-footer ">
-            <p class="mui-ellipsis-2">大范甘迪水电费斯蒂芬森分适当放松的的否是方式方式的否是放是</p>
-          </div>
-          <!-- 商品价格 -->
-          <div class="mui-card-content">
-            <p class="price">
-            	<span>￥233</span>
-            	<s>￥2342</s>
-            </p>
-            <p class="tip">
-            	<span>热卖中</span>
-            	<span>剩余5654件</span>
-            </p>
-          </div>
-        </div>
-      </li>
     </ul>
     <!-- 加载更多 -->
-    <button class="mui-btn mui-btn-success mui-btn-block mui-btn-outlined">加载更多</button>
+    <button class="mui-btn mui-btn-success mui-btn-block mui-btn-outlined" ref="loadMoreBtn" @click="getList">加载更多</button>
   </article>
 </template>
 
 <script>
+  import config from '../../js/config.js';
+  import Ctitle from '../common/title.vue';
+
+  export default {
+    data() {
+      return {
+        title: '商品列表',
+        pageindex: 1,
+        list: []
+      };
+    },
+    methods: {
+       // 获取商品列表
+       getList() {
+         let url = config.goodsList + '?pageindex=' + this.pageindex;
+         this.$http.get(url).then(rep => {
+            let body = rep.body;
+            // 因为有加载更多按钮，一页一页累加数据
+            if(body.status == 0 && body.message.length > 0) {
+              this.list.push(...body.message);
+              this.pageindex++;
+            }
+            // 如果已经是最后一页数据了，那么把按钮disable掉
+            else if(body.message.length == 0){
+              this.$refs.loadMoreBtn.disabled = true;
+            }
+         });
+       }
+    },
+    created() {
+      this.getList();
+    },
+    components: {
+      'v-title': Ctitle
+    }
+  };
 </script>
 
 <style lang="less">
