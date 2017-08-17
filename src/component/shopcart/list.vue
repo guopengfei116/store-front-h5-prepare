9c01528877726d5eb9df4bb52069b3ebecfeec16
PengfeiGuo <626138959@qq.com>
2017-08-17 11:56:17 +0800

    购物车列表数据动态化&总价计算

---
+++ src/component/shopcart/list.vue
@@ -0,0 +1,170 @@
+<template>
+  <article class="shopcart-list">
+
+    <v-title :title="title"></v-title>
+
+    <!-- 商品列表 -->
+    <div class="goods" v-for="item in shopcartList" :key="item.id">
+      <mt-switch class="switch" v-model="item.selected"></mt-switch> 
+      <img class="img" :src="item.src">
+      <div class="inforight">
+        <h4 class="mui-ellipsis-2">{{ item.title }}</h4>
+        <div class="bottom">
+          <ul>
+            <li>￥{{ item.sell_price }}</li>
+            <li>
+              <div class="mui-numbox"> <button class="mui-btn mui-btn-numbox-minus">-</button> <input class="mui-input-numbox" type="number"> <button class="mui-btn mui-btn-numbox-plus">+</button> </div>
+            </li>
+            <li>
+              <a href="javascript:void(0)">删除</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <!-- 总价 -->
+    <div class="total">
+      <div class="total_val">
+        <ul>
+          <li>总计（不含运费）</li>
+          <li>已勾选商品 {{ sumTotal }} 件,总价:￥ {{ priceTotal }} 元</li>
+        </ul>
+      </div>
+      <div class="total_btn">
+        <mt-button type="primary">付 款</mt-button>
+      </div>
+    </div>
+
+  </article>
+</template>
+
+<script>
+  import config from '../../js/config.js';
+  import Ctitle from '../common/title.vue';
+  import goodsStorage from '../../js/model/goods.js';
+
+  export default {
+
+   data() {
+     return {
+      title: '购物车',
+      shopcartList: []
+     };
+   },
+
+   // 计算属性
+   computed: {
+
+     // 总数量
+     sumTotal() {
+        // 遍历取出每个商品的数量，如果这个商品是未选中状态，那么数量设为0
+        let totalArr = this.shopcartList.map(item => item.selected? +goodsStorage.get(item.id): 0);
+        return totalArr.length && totalArr.reduce((v1, v2) => v1 + v2);
+     },
+
+     // 总价格
+     priceTotal() {
+       let priceArr = this.shopcartList.map(item => item.selected? +goodsStorage.get(item.id) * item.sell_price: 0);
+       return priceArr.length && priceArr.reduce((v1, v2) => v1 + v2);
+     }
+   },
+
+   methods: {
+     // 获取购物车商品数据
+     getShopcartList() {
+
+      // 如果之前没有选购过商品，就不用请求接口了
+      let idList = goodsStorage.getIDList();
+      if(!idList.length) { return; }
+
+      // 有的话再请求
+      let url = config.shopcartList + idList;
+      this.$http.get(url).then(rep => {
+        let body = rep.body;
+        // 因为请求回来的图片没有域名，所以加一下
+        // 每个商品都额外添加一个selected属性用来控制该商品的选取，默认选取状态
+        if(body.status == 0) {
+          this.shopcartList = body.message.map(item => {
+            item.selected = true;
+            item.src = config.imgDomain + item.thumb_path;
+            return item;
+          });
+        }
+      });
+     }
+   },
+
+   created() {
+     this.getShopcartList();
+   },
+
+   components: {
+     'v-title': Ctitle
+   }
+
+ };
+</script>
+
+<style lang="less">
+  .shopcart-list {
+    .goods {
+      border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+      height: 102px;
+      display: flex;
+      padding: 5px;
+      .switch {
+        flex: 0 0 52px;
+      }
+      .img {
+        margin-left: 5px;
+        height: 75px;
+        width: 75px;
+        flex: 0 0 85px;
+      }
+      .inforight {
+        margin-left: 5px;
+        flex: 1;
+      }
+      .inforight ul {
+        padding-left: 0px;
+      }
+      .inforight li {
+        list-style: none;
+        display: inline-block;
+      }
+      .inforight h4 {
+        color: #0094ff;
+        font-size: 14px;
+      }
+      .bottom li:first-child {
+        color: red;
+        margin-right: 5px;
+      }
+      .bottom li:last-child {
+        margin-left: 5px;
+      }
+    }
+    .total {
+      height: 84px;
+      background-color: rgba(0, 0, 0, 0.1);
+      display: flex;
+      padding: 5px 14px;
+      ul {
+		    padding-left: 0px;
+		    margin: 14px 0;
+		    li {
+		      list-style: none;
+		      font-size: 14px;
+		    }
+		  }
+		  &_val {
+		  	flex: 1;
+		  }
+		  &_btn {
+		  	flex: 0 0 60px;
+	      margin: 18px 0 0 0;
+		  }
+    }
+  }
+</style>
---
+++ src/component/shopcart/tpl/list_tpl.vue
@@ -0,0 +1,103 @@
+<template>
+  <article class="shopcart-list">
+
+    <!-- 商品列表 -->
+    <div class="goods">
+      <mt-switch class="switch"></mt-switch> <img class="img" src="">
+      <div class="inforight">
+        <h4 class="mui-ellipsis-2">小米斯蒂芬斯蒂芬适当当释放斯蒂芬斯蒂芬斯放倒第三放松的</h4>
+        <div class="bottom">
+          <ul>
+            <li>￥6666</li>
+            <li>
+              <div class="mui-numbox"> <button class="mui-btn mui-btn-numbox-minus">-</button> <input class="mui-input-numbox" type="number"> <button class="mui-btn mui-btn-numbox-plus">+</button> </div>
+            </li>
+            <li>
+              <a href="javascript:void(0)">删除</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <!-- 总价 -->
+    <div class="total">
+      <div class="total_val">
+        <ul>
+          <li>总计（不含运费）</li>
+          <li>已勾选商品10件,总价:￥1000元</li>
+        </ul>
+      </div>
+      <div class="total_btn">
+        <mt-button type="primary">付 款</mt-button>
+      </div>
+    </div>
+
+  </article>
+</template>
+
+<script>
+</script>
+
+<style lang="less">
+  .shopcart-list {
+    .goods {
+      border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+      height: 102px;
+      display: flex;
+      padding: 5px;
+      .switch {
+        flex: 0 0 52px;
+      }
+      .img {
+        margin-left: 5px;
+        height: 75px;
+        width: 75px;
+        flex: 0 0 85px;
+      }
+      .inforight {
+        margin-left: 5px;
+        flex: 1;
+      }
+      .inforight ul {
+        padding-left: 0px;
+      }
+      .inforight li {
+        list-style: none;
+        display: inline-block;
+      }
+      .inforight h4 {
+        color: #0094ff;
+        font-size: 14px;
+      }
+      .bottom li:first-child {
+        color: red;
+        margin-right: 5px;
+      }
+      .bottom li:last-child {
+        margin-left: 5px;
+      }
+    }
+    .total {
+      height: 84px;
+      background-color: rgba(0, 0, 0, 0.1);
+      display: flex;
+      padding: 5px 14px;
+      ul {
+		    padding-left: 0px;
+		    margin: 14px 0;
+		    li {
+		      list-style: none;
+		      font-size: 14px;
+		    }
+		  }
+		  &_val {
+		  	flex: 1;
+		  }
+		  &_btn {
+		  	flex: 0 0 60px;
+	      margin: 18px 0 0 0;
+		  }
+    }
+  }
+</style>
