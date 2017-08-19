655b6fb3a5ddb71a0777d17a6a00b2ecb321e3fd
PengfeiGuo <626138959@qq.com>
2017-08-19 10:38:31 +0800

    添加vuex库，以及vuex数据在任何组件可以共享的演示

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
+import Vuex from 'vuex';
 import MintUI from 'mint-ui';    // MintUI是vue插件，将来需要手动use才可以使用
 import 'mint-ui/lib/style.css';  // 引入样式库还需单独导入css文件
 import 'mui/dist/css/mui.css';    // 引入mui样式
@@ -10,6 +11,7 @@ import 'mui/examples/hello-mui/css/icons-extra.css'; // 引入mui的扩展图标
 // 1.1、记得手动use启用插件
 Vue.use(VueRouter);
 Vue.use(VueResource);
+Vue.use(Vuex);
 Vue.use(MintUI);
 
 // 二、导入我们自己的东西
@@ -32,5 +34,37 @@ import router from './router.js';  // 路由
 new Vue({
     el: '#app',
     render: c => c(App),
-    router
+    router,
+    store: new Vuex.Store({
+        // 相当于咱们之前的data配置
+        state: {
+            count: 200
+        },
+        // 相当于咱们之前的computed计算属性，使用方式也一样
+        getters: {
+
+            // 提供一个获取数据方法，这个方法会接收到state数据对象，
+            // 由我们自由去返回数据，也可以对数据进行加工后再返回
+            getCount(state) {
+                return state.count;
+            }
+        },
+        // 相当于咱们之前的methods，不过这里的方法仅仅是对数据进行改变，
+        // 然后还有一个却别就是调用方式不一样，不能使用方法名直接调用这个方法，
+        // 必须通过组件实例this.$store.commit(mutations名字)的方式间接调用该方法。
+        // 为什么这么复杂：1、vue官方提供的插件要追踪值的变化，必须这样用，否则追踪不到。
+        // 为什么这么复杂：2、下面我们写的修改数据方法，vuex会进行二次封装，以给我们提供一些额外的数据
+        mutations: {
+            
+            // 修改count方式1，自增
+            countAdd(state) {
+                state.count++;
+            },
+
+            // 修改count方式2，覆盖
+            setCount(state, val) {
+                state.count = val;
+            }
+        }
+    })
 });
